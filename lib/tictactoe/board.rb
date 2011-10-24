f3a18f26539edc320a2bb1878b0d739a47480eb4
Glenn Goodrich <glenn.goodrich@gmail.com>
2011-10-24 10:22:50 -0400

    Fighting has_winner logic

--- lib/tictactoe/board.rb
+++ lib/tictactoe/board.rb
@@ -9,9 +9,9 @@ module TicTacToe
     end
    
     def rows
-      @surface
+      [@surface[0],@surface[1],@surface[2] ] 
     end
-   
+
     def cols
       col1 = []
       col2 = []
@@ -61,21 +61,19 @@ module TicTacToe
 
     def has_winner?
       winner = false
-      @surface.each do |row|
+      [rows].each do |row|
+        row.each do |cells|
+          if cells.all? {|c| c == :X} || cells.all? {|c| c == :O}
+            winner = true
+            break
+          end
+        end
+      end
+      [cols, left_diagonal, right_diagonal].each do |row|
         if row.all? {|c| c == :X} || row.all? {|c| c == :O}
           winner = true
+          break
         end
-      end
-      cols.each do |col|
-        if col.all? {|c| c == :X} || col.all?{|c| c == :O}
-          winner = true
-        end
-      end
-      if left_diagonal.all? {|c| c == :X} || left_diagonal.all?{|c| c == :O}
-        winner = true
-      end
-      if right_diagonal.all? {|c| c == :X} || right_diagonal.all?{|c| c == :O}
-        winner = true
       end
       winner
     end
